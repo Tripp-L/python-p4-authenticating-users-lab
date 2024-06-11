e9e29b6d8a43be59e40d5e21bb63c0369c64429b
trippl <tlaursen8@yahoo.com>
2024-06-10 21:40:13 -0400

    Authenticating users Lab

--- server/app.py
+++ server/app.py
@@ -13,45 +13,66 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
-
 api = Api(app)
 
 class ClearSession(Resource):
-
     def delete(self):
-    
         session['page_views'] = None
         session['user_id'] = None
-
         return {}, 204
 
 class IndexArticle(Resource):
-    
     def get(self):
         articles = [article.to_dict() for article in Article.query.all()]
         return articles, 200
 
 class ShowArticle(Resource):
-
     def get(self, id):
         session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
         session['page_views'] += 1
-
         if session['page_views'] <= 3:
-
             article = Article.query.filter(Article.id == id).first()
             article_json = jsonify(article.to_dict())
-
             return make_response(article_json, 200)
-
         return {'message': 'Maximum pageview limit reached'}, 401
 
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
 
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data['username']
+        user = User.query.filter_by(username=username).first()
+        if user:
+            session['user_id'] = user.id
+            return make_response(jsonify(user.to_dict()), 200)
+        return {'message': 'User not found'}, 404
+
+api.add_resource(Login, '/login')
+
+class Logout(Resource):
+    def delete(self):
+        session.pop('user_id', None)
+        return {'message': 'Logged out successfully'}, 204
+
+api.add_resource(Logout, '/logout')
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = db.session.get(User, user_id)
+            if user:
+                return make_response(jsonify(user.to_dict()), 200)
+            else:
+                return make_response(jsonify({}), 404)
+        else:
+            return make_response(jsonify({}), 401)
+
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
